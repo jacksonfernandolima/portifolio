06cdce9efdca25736a0c9fcf28d1e1e7b5123844
jacksonfernandolima <148342108+jacksonfernandolima@users.noreply.github.com>
2023-11-15 21:19:00 +0000

    contatos

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -2,7 +2,12 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Container from "../../components/Container";
 import styles from "./Contatos.module.css";
-import { LuMail, BsInstagram, SlSocialYoutube, LuGithub, AiOutlineLinkedin } from "react-icons/lu";
+import { LuMail } from "react-icons/lu";
+import { LuInstagram } from "react-icons/lu";
+import { LuYoutube } from "react-icons/lu";
+import { LuGithub } from "react-icons/lu";
+import { LuLinkedin } from "react-icons/lu";
+
 
 function Contatos() {
     return (
@@ -16,11 +21,26 @@ function Contatos() {
                     <p>Para que possamos conversar mais sobre.</p>
 
                     <div className={styles.icones}>
-                        <LuMail />
-                        <BsInstagram />
-                        <SlSocialYoutube />
-                        <LuGithub />
-                        <AiOutlineLinkedin />
+
+                        <a href="mailto:jacksonfernando_lima@outlook.com" target="_blank" rel="noopener noreferrer"> 
+                        <LuMail  className={styles.icone}/>
+                        </a>
+
+                        <a href="https://www.instagram.com/jackson.devwork/" target="_blank" rel="noopener noreferrer">
+                        <LuInstagram className={styles.icone}/>
+                        </a>
+
+                        <a href="mailto:jacksonfernando_lima@outlook.com" target="_blank" rel="noopener noreferrer">
+                        <LuYoutube className={styles.icone}/>
+                        </a>
+
+                        <a href="https://github.com/jacksonfernandolima" target="_blank" rel="noopener noreferrer">
+                        <LuGithub className={styles.icone}/>
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/jackson-fernando-8597911ba/" target="_blank" rel="noopener noreferrer">
+                        <LuLinkedin className={styles.icone}/>
+                        </a>
 
                     </div>
                 </section>
